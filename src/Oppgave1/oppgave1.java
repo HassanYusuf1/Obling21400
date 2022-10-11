ff1399838a1075760b3f4c06156cdeca16ab6121
HassanYusuf1 <112115781+HassanYusuf1@users.noreply.github.com>
2022-10-11 22:02:55 +0200

    Nesten ferdig med oppgave1

---
+++ src/Oppgave1/oppgave1.java
@@ -0,0 +1,35 @@
+package Oppgave1;
+
+import static javax.swing.JOptionPane.showInputDialog;
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class oppgave1 {
+    public static void main(String []args) {
+        int nedreTall = Integer.parseInt(showInputDialog("skriv tall 1:"));
+        int øvreTall = Integer.parseInt(showInputDialog("skriv tall 2"));
+        int sum = 0;
+        int antall = 0;
+        for (int i = nedreTall; i<=øvreTall; i++) {
+            System.out.print(i);
+            sum += i;
+            antall++;
+            if (i < øvreTall) {
+                System.out.print(" + ");}
+        }
+        System.out.print(" = "+sum);
+
+        if (nedreTall > øvreTall) {
+            showMessageDialog(null, "skriv nytt Tall");
+
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
---
+++ src/oppgave2/oppgave2.java
@@ -0,0 +1,13 @@
+package oppgave2;
+
+import static javax.swing.JOptionPane.showInputDialog;
+
+public class oppgave2 {
+    public static void main(String[]args){
+
+
+
+
+
+    }
+}
